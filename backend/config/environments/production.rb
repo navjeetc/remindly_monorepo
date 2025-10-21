96fc9e5d3e2af23c0c99cffe58ddd532fe1cf885
Navjeet Chabbewal <navjeetc@gmail.com>
2025-10-20 23:04:43 -0400

    Enable raise_delivery_errors to debug email issues

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -50,9 +50,8 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Raise email delivery errors so we can see what's wrong
+  config.action_mailer.raise_delivery_errors = true
 
   # Configure Postmark for email delivery
   config.action_mailer.delivery_method = :postmark
