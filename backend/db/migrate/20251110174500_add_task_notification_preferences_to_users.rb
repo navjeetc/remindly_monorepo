20416648a784a5be0238f6ccbb22164f9667cf49
Navjeet Chabbewal <navjeetc@gmail.com>
2025-11-10 17:05:16 -0500

    Fix migration version to match Rails 8.0
    - Update migration version from 7.2 to 8.0
    - Matches Rails version in schema.rb

--- backend/db/migrate/20251110174500_add_task_notification_preferences_to_users.rb
+++ backend/db/migrate/20251110174500_add_task_notification_preferences_to_users.rb
@@ -1,4 +1,4 @@
-class AddTaskNotificationPreferencesToUsers < ActiveRecord::Migration[7.2]
+class AddTaskNotificationPreferencesToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :notify_on_task_assigned_to_others, :boolean, default: false
   end
